5702bb1d5c8d7905cd53b503901d900a00be096f
Administrator <Administrator@172.31.14.152>
2024-04-18 07:30:05 +0000

    Change 2

--- src/test/java/testscripts/googlepagetest.java
+++ src/test/java/testscripts/googlepagetest.java
@@ -1,6 +1,9 @@
 package testscripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class googlepagetest {
@@ -10,6 +13,9 @@ public class googlepagetest {
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
 		System.out.println("Page Title: " + driver.getTitle());
+		WebElement srchBox = driver.findElement(By.id("APjFqb"));
+		srchBox.sendKeys("Java Tutorial");
+		srchBox.sendKeys(Keys.ENTER);
 
 	}
 
